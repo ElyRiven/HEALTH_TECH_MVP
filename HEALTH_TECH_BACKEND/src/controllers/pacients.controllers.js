0e44e7f7eac1fffab615c7e9ed06c6c0dd5bc7f0
Juan David Franco <jfrancoh5@gmail.com>
2026-03-28 10:49:43 -0500

    (feat): implement PatientTable component with sorting functionality and integrate into Dashboard

--- HEALTH_TECH_BACKEND/src/controllers/pacients.controllers.js
+++ HEALTH_TECH_BACKEND/src/controllers/pacients.controllers.js
@@ -254,7 +254,19 @@ export const GetAllPacients = async (req, res) => {
         //const order = req.query.order?.toUpperCase() === 'ASC' ? 'ASC' : 'DESC';
         console.log(`[BACKEND] GetAllPacients called with order: ${order}`);     
 
-        const queryText = `SELECT * FROM public.pacientes ORDER BY criticidad ${order}`;
+        const queryText = `
+            SELECT
+                identificacion,
+                nombres,
+                apellidos,
+                TO_CHAR(fecha_de_nacimiento, 'YYYY-MM-DD') AS fecha_de_nacimiento,
+                genero,
+                criticidad,
+                TO_CHAR(hora_de_registro::timestamptz, 'HH12:MI:SS AM') AS hora_de_registro,
+                estado
+            FROM public.pacientes
+            ORDER BY criticidad ${order}
+        `;
         const { rows } = await pool.query(queryText);
 
         res.status(200).json(rows);
---
+++ HEALTH_TECH_FRONTEND/src/hooks/useGetListPacients/types.ts
@@ -0,0 +1,17 @@
+export interface Pacient {
+  identificacion: number;
+  nombres: string;
+  apellidos: string;
+  fecha_de_nacimiento: string;
+  genero: string;
+  criticidad: number;
+  hora_de_registro: string;
+  estado: string;
+}
+
+export interface UseGetListPacients {
+  pacients: Pacient[];
+  loading: boolean;
+  error: string | null;
+  fetchPacients: (order?: 'ASC' | 'DESC') => Promise<void>;
+}
