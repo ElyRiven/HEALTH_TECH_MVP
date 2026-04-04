7378a91a575708b9d5b2314d663f100f249859cf
Elizabeth Tamayo <dianelizbty@gmail.com>
2026-04-03 21:33:57 -0500

    refactor: update database pool configuration to define SSL connection in producton

--- HEALTH_TECH_BACKEND/src/db.js
+++ HEALTH_TECH_BACKEND/src/db.js
@@ -1,11 +1,22 @@
-import dotenv from 'dotenv';
-import pg from 'pg';
+import dotenv from "dotenv";
+import { Pool } from "pg";
+
 dotenv.config();
 
-export const pool = new pg.Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    password: String(process.env.DB_PASSWORD),
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
-})
+const dbConfig = {
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: String(process.env.DB_PASSWORD),
+  database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT || "5432", 10),
+};
+
+console.log("Current STAGE:", process.env.STAGE);
+
+if (process.env.STAGE === "production") {
+  dbConfig.ssl = {
+    rejectUnauthorized: false,
+  };
+}
+
+export const pool = new Pool(dbConfig);
